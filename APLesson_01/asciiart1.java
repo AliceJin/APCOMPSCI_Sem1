08eeb2b3cdbaddc495a61fd63a28523ace1c661a
AliceJin <snowjay99@gmail.com>
2016-09-06 08:02:36 -0700

    Added Files

---
+++ APLesson_01/asciiart1.java
@@ -0,0 +1,23 @@
+public class asciiart1
+{
+	public static void main(String[]args)
+	{
+		System.out.println("     ____//     ");
+		System.out.println("    / .___.\\   ");
+		System.out.println("    \\  \\ / /  ");
+		System.out.println("\'__/       \\  ");
+		System.out.println(" \\-         )  ");
+		System.out.println("  \\________/   ");
+		System.out.println("______|__|______");
+		System.out.println("      \"  \"    \n\n\n");
+		
+		System.out.println("      -=-         ");
+		System.out.println("  (\\  __   /)    ");
+		System.out.println("  (\'\\ (\") /\') ");
+		System.out.println("  (\'/ <V> \\\')  ");
+		System.out.println("   \\\\ /^\\ //   ");
+		System.out.println("     /   \\       ");
+		System.out.println("    /     \\      ");
+		System.out.println("   \'- ._. -\'    ");
+	}
+}
