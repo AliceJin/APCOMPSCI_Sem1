e11995062c6a2fa6b4883ab02bb4ccce5f653400
AliceJin <snowjay99@gmail.com>
2016-10-28 09:18:05 -0700

    added quiz_3 files

---
+++ quiz3.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class quiz3
+{
+	public static void main(String[]args)
+	{
+		int total = 3;
+		int MAX = 3;
+		int sum = 1;
+		
+		if(total == MAX)
+		{
+			if(total < sum)
+				System.out.println("total == MAX and is <sum. ");
+		}
+		else
+			System.out.println("total is not equal to MAX");
+	}
+}
+
+//question 2: first part false and && so automatically else
+//if order switch, then exception
+
+//question 3: 22 is negative; last two if-else group together
+
+//question 4: y = 90; the else matches with the innermost if, even if there are brackets
+
+//question 5: E
+
+//question 6: D
+
+//question 7: A 
+
+//question 8:
+
+//question 9: D. no
+
+//question 10: add brackets???, idk why didn't work with q4
+
