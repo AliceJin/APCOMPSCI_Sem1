9a53234c0386284d79bc002d38792632fdc79577
AliceJin <snowjay99@gmail.com>
2016-12-05 09:18:31 -0800

    Added Lesson_11 part 1

---
+++ APLesson_11/Lesson_11.java
@@ -0,0 +1,30 @@
+public class Lesson_11
+{
+	public static void main(String[]args)
+	{
+		int [][] values = new int [4][4];
+		int num = 1;
+		
+		//filling it vertically
+		for(int i = 0; i < values.length; i++)
+		{
+			for(int j = 0; j < values[i].length; j++)
+			{
+				values[j][i] = num;
+				num++;
+			}
+		}
+		
+		//printing
+		for(int i = 0; i < values.length; i++)
+		{
+			for(int j = 0; j < values[i].length; j++)
+			{
+				System.out.print(values[i][j] + "\t");
+			}
+			System.out.println();
+		}
+		
+		//search the array
+	}
+}
