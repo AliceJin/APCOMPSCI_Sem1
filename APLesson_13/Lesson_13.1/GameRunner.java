624843be31d1fe2b30ba99baf50161e91fd2ca98
AliceJin <snowjay99@gmail.com>
2017-02-28 08:19:57 -0800

    Added Lesson 13.1 files

--- APLesson_13/Lesson_13.1/GameRunner.java
+++ APLesson_13/Lesson_13.1/GameRunner.java
@@ -2,12 +2,12 @@ public class GameRunner
 {
 	public static void main(String[]args)
 	{
-		XBox t1 = new XBox();
-		XBox t2 = new XBox("sdkd");
-		PlayStation t3 = new PlayStation();
-		PlayStation t4 = new PlayStation("sdkd");
-		PC t5 = new PC();
-		PC t6 = new PC("sdkd");
+		GameSystem t1 = new XBox();
+		GameSystem t2 = new XBox("sdkd");
+		GameSystem t3 = new PlayStation();
+		GameSystem t4 = new PlayStation("sdkd");
+		GameSystem t5 = new PC();
+		GameSystem t6 = new PC("sdkd");
 		
 		System.out.println(t1);
 		System.out.println(t2);
