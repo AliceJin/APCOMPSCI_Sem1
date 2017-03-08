4d2fbdf63d939f7d0e824cd7b99975db6730c7d4
AliceJin <snowjay99@gmail.com>
2017-03-08 09:20:09 -0800

    Added Lesson 14 files

---
+++ APLesson_14/Video notes/Circle.java
@@ -0,0 +1,18 @@
+public class Circle
+{
+	private double radius;
+	public Circle()
+	{
+		radius = 0;
+	}
+	
+	public Circle(double r)
+	{
+		radius = r;
+	}
+	
+	public double getRadius()
+	{
+		return radius;
+	}
+}
---
+++ APLesson_14/Video notes/CutShapes.java
@@ -0,0 +1,1 @@
+//interface - collection of abstract methods
